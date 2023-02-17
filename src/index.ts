46a507a8efa0a6182ca9986fa28fa3a6e48e415b
Max Holman <max@holmn.com>
2023-02-17 09:20:11 +0800

    fix: export types to assist with resolution on client side

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@
 export { ServiceError } from '../lib/errors.js';
 
 // for building your own fetcher/fetcher factory
-export type { FetcherMethod } from '../lib/fetcher.js';
+export type {
+  FetcherMethod,
+  FetcherResponse,
+  FetcherParams,
+} from '../lib/fetcher.js';
 
 // a good standard/basic fetcher factory
 export { createIsomorphicFetcher } from './isomorphic-fetch.js';
